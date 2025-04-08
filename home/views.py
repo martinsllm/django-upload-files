069237b024a5922004e580481ee3f0a2b4dd09bb
martinsllm <lucaslm2010@gmail.com>
2025-04-08 19:18:09 -0300

    Uploading images

--- home/views.py
+++ home/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from PIL import Image
+import os
+from django.conf import settings
 
 
 def home(request):
     if request.method == "GET":
         return render(request, 'home.html')
     elif request.method == "POST":
-        file = request.FILES
-        print(file)
+        file = request.FILES.get('file')
+        img = Image.open(file)
+        path = os.path.join(settings.MEDIA_ROOT, file.name)
+        img = img.save(path)
         return HttpResponseRedirect("/")
